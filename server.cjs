69d83e3d8d897a33e3047012a73e2a4682fdd01a
Jay Rome Rana <itaalyyy@gmail.com>
2024-09-29 20:20:23 +0800

    added user endpoint

--- server.cjs
+++ server.cjs
@@ -6,6 +6,15 @@ app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
 
+app.get('/users', (req, res) => {
+	const users = [
+		{ id: 1, name: 'John' },
+		{ id: 2, name: 'Jane' },
+		{ id: 3, name: 'Joe' }
+	];
+	res.json(users);
+});
+
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`);
 });
